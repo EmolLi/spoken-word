09348d17f63c43555be90bf1d4ad5c99cad0d39f
Feilan Jiang <fjiang120@gmail.com>
2017-01-29 09:24:05 -0500

    syntax

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const path = require('path')
 const url = require('url')
 let fs = require('fs');
 let shell = require('shelljs');
-let DATA = require('./Data.js);
+let DATA = require('./Data.js');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
